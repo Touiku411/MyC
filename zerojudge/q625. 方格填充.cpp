e0d544d9720baeb50e9956dfd330b7af82fa32e1
Touiku411 <tengyu081229@gmail.com>
2025-09-12 01:30:31 +0800

    Update q625. 方格填充.cpp

--- zerojudge/q625. 方格填充.cpp
+++ zerojudge/q625. 方格填充.cpp
@@ -7,7 +7,7 @@ int fill(int n, int m) {
 	if (n == 0 || m == 0)return 0;
 	int k = 1;
 	while ((k * 2 <= m) && (k * 2 <= n))k *= 2;
-	return 1 + fill(m - k, k) + fill(n - k, m);
+	return 1 + fill( k, m - k) + fill(n - k, m);
 
 }
 int main()
