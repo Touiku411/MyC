ab36d368d783a680cbd4b33caaea1ff3ebb78eb6
Touiku411 <tengyu081229@gmail.com>
2025-10-19 00:36:39 +0800

    Update Armstrong Numbers.cpp

--- codingpass/05-Recursive Functions/Armstrong Numbers.cpp
+++ codingpass/05-Recursive Functions/Armstrong Numbers.cpp
@@ -1,21 +1,14 @@
 #include<iostream>
 using namespace std;
 
+// returns the sum of the k-th powers of digits of n,
+// where k is the number of digits of n.
+int sumPowerDigits(int n);
 
+// returns the p-th power of d
+int power(int d, int p);
 
 int numDigits; // the number of digits of n
-// returns the p-th power of d
-int power(int d, int p)
-{
-    if (p == 0)return 1;
-    return d * power(d, p - 1);
-}
-int sumPowerDigits(int n)
-{
-    if (n == 0)return 0;
-    else
-        return power(n % 10, numDigits) + sumPowerDigits(n / 10);
-}
 
 int main()
 {
@@ -37,3 +30,15 @@ int main()
     }
 }
 
+int sumPowerDigits(int n)
+{
+    if (n == 0)return 0;
+    return power(n % 10, numDigits) + sumPowerDigits(n / 10);
+}
+
+// returns the p-th power of d
+int power(int d, int p)
+{
+    if (p == 0)return 1;
+    return d * power(d, p - 1);
+}
