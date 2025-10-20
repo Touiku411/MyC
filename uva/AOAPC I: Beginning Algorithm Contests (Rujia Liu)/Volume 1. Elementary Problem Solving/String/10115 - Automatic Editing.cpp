7ca9fc1fbb65edee9ceb59a45b3afc6111926b08
Touiku411 <tengyu081229@gmail.com>
2025-10-20 18:04:46 +0800

    Update 10115 - Automatic Editing.cpp

--- uva/AOAPC I: Beginning Algorithm Contests (Rujia Liu)/Volume 1. Elementary Problem Solving/String/10115 - Automatic Editing.cpp
+++ uva/AOAPC I: Beginning Algorithm Contests (Rujia Liu)/Volume 1. Elementary Problem Solving/String/10115 - Automatic Editing.cpp
@@ -21,18 +21,12 @@ int main() {
 		}
 		string word;
 		getline(cin, word);
-		string newWord;
 
 		for (auto i : Rules) {
 			while (word.find(i.first) != string::npos) {
 				size_t pos = word.find(i.first);
 				int len = i.first.size();
-				if (pos == 0)
-					newWord = i.second + word.substr(pos + len);
-				else
-					newWord = word.substr(0, pos) + i.second + word.substr(pos + len);
-				word = newWord;
-				newWord.clear();
+				word.replace(pos, len, i.second);
 			}
 		}
 		cout << word << '\n';
