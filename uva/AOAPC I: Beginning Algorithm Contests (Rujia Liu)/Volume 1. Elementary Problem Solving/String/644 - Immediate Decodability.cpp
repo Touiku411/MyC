254287d031793472fe057f9320c856283f623a4f
Touiku411 <tengyu081229@gmail.com>
2025-10-21 09:04:53 +0800

    Create 644 - Immediate Decodability.cpp

---
+++ uva/AOAPC I: Beginning Algorithm Contests (Rujia Liu)/Volume 1. Elementary Problem Solving/String/644 - Immediate Decodability.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+#include<set>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	string input;
+	set<string> code;
+	bool t = true;
+	int cnt = 0;
+	while (cin >> input) {
+		if (input == "9") {
+			cout << "Set " << ++cnt;
+			if (t)
+				cout << " is immediately decodable\n";
+			else
+				cout << " is not immediately decodable\n";
+			t = true;
+			code.clear();
+			continue;
+		}
+		for (auto &i : code) {
+			if (input.rfind(i, 0) == 0 || i.rfind(input, 0) == 0){
+				t = false;
+				break;
+			}
+		}
+		code.insert(input);
+	}
+}
+
