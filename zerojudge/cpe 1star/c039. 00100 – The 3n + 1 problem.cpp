122f9dc0bb479863b52347898f2f8cfe08e91699
Touiku411 <tengyu081229@gmail.com>
2025-11-29 03:56:21 +0800

    Update c039. 00100 – The 3n + 1 problem.cpp

--- zerojudge/cpe 1star/c039. 00100 – The 3n + 1 problem.cpp
+++ zerojudge/cpe 1star/c039. 00100 – The 3n + 1 problem.cpp
@@ -30,4 +30,28 @@ int main() {
         cout << i << " " << j << " " << maxlen << endl;
     }
 }
+// 遞迴寫法
+// int countLen(int n,int cnt) {
+//     if (n == 1)return cnt;
+//     if (n % 2 != 0)
+//         return cnt + countLen(n * 3 + 1, cnt);
+//     else 
+//         return cnt + countLen(n / 2, cnt);
+// }
+// int main() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(0);
+//     int i, j;
+//     while (cin >> i >> j) {
+//         int start = min(i, j);
+//         int end = max(i, j);
+//         int maxlen = 0;
+//         int cnt = 1;
+//         for (int k = start; k <= end; ++k) {
+//             maxlen = max(maxlen, countLen(k , cnt));
+//         }
+//         cout << i << " " << j << " " << maxlen << endl;
+//     }
+// }
     
+
