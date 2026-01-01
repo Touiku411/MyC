118bb00942570f29292ce72275aeba940e57fdca
Touiku411 <tengyu081229@gmail.com>
2026-01-01 21:59:01 +0800

    Update as13.cpp

--- codingpass/as13.cpp
+++ codingpass/as13.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <iomanip>
 #include <fstream>
@@ -162,7 +163,6 @@ void loadMovies(Movie movies[], int& numMovies)
         system("pause");
         exit(1);
     }
-    Movie m;
     numMovies = 0;
     while (inFile.read(reinterpret_cast<char*>(&movies[numMovies]), sizeof(Movie))) {
         numMovies++;
@@ -192,7 +192,7 @@ void loadAvailableDates(char availableDates[][12], int& numDates)
         exit(1);
     }
     numDates = 0;
-    while (inFile.getline(availableDates[numDates], 10)) {
+    while (inFile.getline(availableDates[numDates], 12)) {
         numDates++;
     }
 }
@@ -285,23 +285,39 @@ bool legal(char email[], char password[], MemberRecord memberDetails[],
             return true;
         }
 
-    cout << "\nSorry, unrecognized email or password.\n\n";
-    return false;
+cout << "\nSorry, unrecognized email or password.\n\n";
+return false;
 }
 
 void accountInfor(MemberRecord memberDetails[], int numMembers, int recordNumber)
 {
     cout << "1. Name: " << memberDetails[recordNumber].name << endl;
-    cout << "2. Phone Number: " << memberDetails[recordNumber].phone << endl;;
-    cout << "3. ID Number: " << memberDetails[recordNumber].IDNumber << endl;;
-    cout << "4. Password: " << memberDetails[recordNumber].password << endl;;
+    cout << "2. Phone Number: " << memberDetails[recordNumber].phone << endl;
+    cout << "3. ID Number: " << memberDetails[recordNumber].IDNumber << endl;
+    cout << "4. Password: " << memberDetails[recordNumber].password << endl;
     int choice;
     do {
         cout << "\nWhich one do you want to modify (0 – not modify): ";
-    } while ((choice = inputAnInteger(1, 4)) == -1);
-
-    
-
+    } while ((choice = inputAnInteger(0, 4)) == -1);
+    if (choice == 0)return;
+    cout << "\nEnter correct data: ";
+    switch (choice) {
+    case 1:
+        cin >> memberDetails[recordNumber].name;
+        break;
+    case 2:
+        cin >> memberDetails[recordNumber].phone;
+        break;
+    case 3:
+        cin >> memberDetails[recordNumber].IDNumber;
+        break;
+    case 4:
+        cin >> memberDetails[recordNumber].password;
+        break;
+    }
+    saveMemberInfo(memberDetails, numMembers);
+    cout << "\nSuccessful!\n\n";
+    cin.ignore();
 }
 
 void newMember(MemberRecord memberDetails[], int& numMembers)
@@ -359,55 +375,175 @@ void buyTickets(BookingInfo bookingHistories[], int& numBookings, Movie movies[]
 {
     displaySessionTimes(movies, movieNames, numMovies, availableDates, numDates);
 
+    int movieCode;
+    int dateCode;
+    int timeCode;
+    int numAdult;
+    int numConcession;
 
+    do {
+        cout << "Enter movie code (0 - 10): ";
+    } while ((movieCode = inputAnInteger(0, 10)) == -1);
+    cout << endl;
 
+    do{
+        cout << "Enter date code (0 - 5): ";
+        dateCode = inputAnInteger(0, 5);
+    } while (dateCode == -1 || !movies[movieCode].dates[dateCode]);
+    cout << endl;
+    do {
+        cout << "Enter session time code (0 - 11): ";
+        timeCode = inputAnInteger(0, 11);
+    } while (timeCode == -1 || !movies[movieCode].sessionTimes[timeCode]);
 
+    cout << endl;
 
+    cout << "Movie: " << movieNames[movieCode] << endl;
+    cout << "Date: " << availableDates[dateCode] << endl;
+    cout << "Show Time: " << hours[timeCode] << endl;
+    cout << "Price: Adult-" << movies[movieCode].prices[0] << ", Concession-" << movies[movieCode].prices[1] << endl;
+
+    cout << endl;
+    do {
+        cout << "Enter the number of adult tickets (0 - 10): ";
+    } while ((numAdult = inputAnInteger(0, 10)) == -1);
+    cout << endl;
+    do {
+        cout << "Enter the number of concession tickets (0 - 10): ";
+    } while ((numConcession = inputAnInteger(0, 10)) == -1);
+
+    bookingHistories[numBookings].dateCode = dateCode;
+    bookingHistories[numBookings].movieCode = movieCode;
+    bookingHistories[numBookings].numTickets[0] = numAdult;
+    bookingHistories[numBookings].numTickets[1] = numConcession;
+    bookingHistories[numBookings].sessionTimeCode = timeCode;
+    strcpy(bookingHistories[numBookings].email, email);
 
     display(movies, bookingHistories[numBookings]);
 
     selectSeats(bookingHistories, numBookings, movies);
-
+    saveBookingHistories(bookingHistories, numBookings);
+    saveMovies(movies, numMovies);
     cout << "\nSuccessful!\n";
-
+    cin.ignore();
     numBookings++;
 }
 
 void selectSeats(BookingInfo bookingHistories[], int numBookings, Movie movies[])
 {
-
-
-
-
-
+    int movieCode = bookingHistories[numBookings].movieCode;
+    int dateCode = bookingHistories[numBookings].dateCode;
+    int timeCode = bookingHistories[numBookings].sessionTimeCode;
+    int numAdult = bookingHistories[numBookings].numTickets[0];
+    int numConcession = bookingHistories[numBookings].numTickets[1];
+    cout << "\n  A B C D E F G H I J K L\n";
+    for (int i = 0; i < 8; ++i) {
+        cout << i << " ";
+        for (int j = 0; j < 12; j++) {
+            if (!movies[movieCode].occupiedSeats[dateCode][timeCode][i][j])
+                cout << "0 ";
+            else
+                cout << "1 ";
+        }
+        cout << endl;
+    }
+    int numTotalTickets = numAdult + numConcession;
+    cout << "\nSelect " << numTotalTickets << " seats(e.g. 0A) :\n";
+    for (int i = 0; i < numTotalTickets; ++i) {   
+        bool taken = false;
+        do {
+            cout << "? ";
+            cin >> bookingHistories[numBookings].seletedSeats[i];
+            char digit1 = bookingHistories[numBookings].seletedSeats[i][0];
+            char digit2 = bookingHistories[numBookings].seletedSeats[i][1];
+            if (movies[movieCode].occupiedSeats[dateCode][timeCode][digit1 - '0'][digit2 - 'A']) {
+                taken = true;
+                cout << "\nThis seat bas been occupied. Please select another seat.\n";
+            }
+            else {
+                taken = false;
+                movies[movieCode].occupiedSeats[dateCode][timeCode][digit1 - '0'][digit2 - 'A'] = true;
+            }
+        } while (taken);
+    }
 }
 
 void displaySessionTimes(Movie movies[], char movieNames[][60], int numMovies,
     char availableDates[][12], int numDates)
 {
-
-
-
-
+    for (int i = 0; i < numMovies; ++i) {
+        cout << setw(2) << right << i << ". Movie: " << movieNames[i] << endl;
+        cout << "    Date:";
+        for (int d = 0; d < numDates; ++d) {
+            if (movies[i].dates[d]) {
+                cout << "  " << d << ". " << availableDates[d] << ",";
+            }
+        }
+        cout << "\n    Session Time:";
+        for (int k = 0; k < 12; ++k) {
+            if (movies[i].sessionTimes[k]) {
+                cout << "  " << k << ". " << hours[k] << ",";
+            }
+        }
+        cout << "\n\n";
+    }
 
 }
 
 void display(Movie movies[], BookingInfo bookingHistory)
 {
+    cout << "\n             No. of Tickets  Price  Subtotal\n";
+    cout << "Adult        " << setw(14) << right << bookingHistory.numTickets[0] << "  " <<
+                               setw(5) << right << movies[bookingHistory.movieCode].prices[0] << "  " <<
+                               setw(8) << right  << bookingHistory.numTickets[0] * movies[bookingHistory.movieCode].prices[0] << endl;
+    cout << "Concession   " << setw(14) << right << bookingHistory.numTickets[1] << "  " <<
+                               setw(5) << right << movies[bookingHistory.movieCode].prices[1] << "  " <<
+                               setw(8) << right << bookingHistory.numTickets[1] * movies[bookingHistory.movieCode].prices[1] << endl;
 
-
-
-
-
+    cout << "\nTotal Amount For Tickets: " << bookingHistory.numTickets[0] * movies[bookingHistory.movieCode].prices[0] + bookingHistory.numTickets[1] * movies[bookingHistory.movieCode].prices[1] << endl;
 }
 
 void displayBookingHistory(BookingInfo bookingHistories[], int numBookings, Movie movies[],
     char movieNames[][60], char availableDates[][12], char email[])
 {
+    bool found = false;
 
+    for (int i = 0; i < numBookings; ++i) {
+        if (strcmp(bookingHistories[i].email, email) == 0) {
+            found = true;
+            break;
+        }
+    }
 
+    if (!found) {
+        cout << "\nNo bookings!\n";
+        return;
+    }
 
+    cout << "\nBooking History:\n";
 
+    for (int i = 0; i < numBookings; ++i) {
+        if (strcmp(bookingHistories[i].email, email) == 0) {
+            int mCode = bookingHistories[i].movieCode;
+            int dCode = bookingHistories[i].dateCode;
+            int tCode = bookingHistories[i].sessionTimeCode;
+
+            cout << "\nMovie: " << movieNames[mCode] << endl;
+            cout << "Date: " << availableDates[dCode] << endl;
+            cout << "Show Time: " << hours[tCode] << endl;
+
+            cout << "Seats:";
+            int totalTickets = bookingHistories[i].numTickets[0] + bookingHistories[i].numTickets[1];
+            for (int s = 0; s < totalTickets; ++s) {
+                cout << "  " << bookingHistories[i].seletedSeats[s];
+            }
+            cout << endl;
+
+            display(movies, bookingHistories[i]);
+
+            cout << "\n----------------------------------------------\n";
+        }
+    }
 
 }
 
@@ -443,6 +579,6 @@ void saveBookingHistories(BookingInfo bookingHistories[], int numBookings)
         exit(1);
     }
     for (int i = 0; i < numBookings; ++i) {
-        outFile.write(reinterpret_cast<char*>(&bookingHistories[i]), sizeof(bookingHistories));
+        outFile.write(reinterpret_cast<char*>(&bookingHistories[i]), sizeof(BookingInfo));
     }
 }
