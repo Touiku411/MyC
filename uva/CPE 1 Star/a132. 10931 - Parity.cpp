e43d6487d319efa7ee8242afa9dbb752679f1509
Touiku411 <tengyu081229@gmail.com>
2026-02-03 03:05:56 +0800

    Update a132. 10931 - Parity.cpp

--- uva/CPE 1 Star/a132. 10931 - Parity.cpp
+++ uva/CPE 1 Star/a132. 10931 - Parity.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
 void bin(int n) {
@@ -29,4 +27,22 @@ int main()
         cout << " is " << num(I) << " (mod 2).\n";
     }
 }
+//2
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+
+int main()
+{
+    int I;
+    while (cin >> I) {
+        if (I == 0) break;
+        bitset<32> b(I);
+        string b_str = b.to_string();
+        b_str = b_str.substr(b_str.find('1'));
+        cout << "The parity of " << b_str << " is " << b.count() << " (mod 2).\n";
+    }
+}
+
 
