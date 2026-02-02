8f59c0420be30db2dc5cf7963273387c0922397b
Touiku411 <tengyu081229@gmail.com>
2026-02-03 00:54:48 +0800

    Update 1201Doom's Day Algorithm.cpp

--- uva/CPE 1 Star/1201Doom's Day Algorithm.cpp
+++ uva/CPE 1 Star/1201Doom's Day Algorithm.cpp
@@ -17,3 +17,24 @@ int main()
         cout << Week[date.tm_wday] << endl;
     }  
 }
+// 2
+#include <iostream>
+#include <ctime>
+using namespace std;
+const string Week[7] = { "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday" };
+const int daysInMonth[] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--) {
+        int M, D;
+        cin >> M >> D;
+        int currentDay = 6;//2011/1/1 is Saturday
+        for (int i = 1; i < M; ++i) {
+            currentDay += daysInMonth[i];
+        }
+        currentDay += D - 1;
+        cout << Week[currentDay % 7] << endl;
+    }  
+}
