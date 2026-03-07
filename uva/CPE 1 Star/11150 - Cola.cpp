10996f50adaab2152e49c70bfb839fa11e63d9e0
Touiku411 <tengyu081229@gmail.com>
2026-03-08 04:03:41 +0800

    Create 11150 - Cola.cpp

---
+++ uva/CPE 1 Star/11150 - Cola.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    while (cin >> n) {
+        int ans = n;
+        int empty = n;
+        while (empty >= 3) {
+            ans += empty / 3;
+            empty = empty / 3 + empty % 3;
+        }
+        if (empty == 2) {
+            ans++;
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
