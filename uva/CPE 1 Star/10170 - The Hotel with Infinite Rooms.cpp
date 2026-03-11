1090c6907cce31b7ace6cb1aa512d79f83c8a404
Touiku411 <tengyu081229@gmail.com>
2026-03-11 13:22:30 +0800

    Update 10170 - The Hotel with Infinite Rooms.cpp

--- uva/CPE 1 Star/10170 - The Hotel with Infinite Rooms.cpp
+++ uva/CPE 1 Star/10170 - The Hotel with Infinite Rooms.cpp
@@ -18,4 +18,26 @@ int main()
     }
     
 }
+#include <iostream>
+//更簡單解法  
+// 1 2 2 3 3 3 4 4 4 4
+//           -
+// 若s d (1, 6) 要輸出 3
+// s d  只要當 s < d 時候，d-=s, s++ ， 當 s >= d 時s就是答案
+// 1 6
+// 2 5
+// 3 3
 
+using namespace std;
+
+
+int main() {
+	long long s, d;
+	while (cin >> s >> d) {
+		while (s < d) {
+			d -= s;
+			s++;
+		}
+		cout << s << endl;
+	}
+}
